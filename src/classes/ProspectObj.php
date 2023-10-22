a3fb6da18f12d3b09fa44440b8b3129fd3413bc1
eleclerc85 <eleclerc@hotmail.ca>
2023-10-22 10:37:22 -0400

    Update ProspectObj.php

--- src/classes/ProspectObj.php
+++ src/classes/ProspectObj.php
@@ -41,7 +41,7 @@ class ProspectObj implements \JsonSerializable{
         $this->name = $name;
     }
 
-    public function jsonSerialize()
+    public function jsonSerialize(): mixed
     {
         return get_object_vars($this);
     }
