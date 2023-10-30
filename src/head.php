b28ba3e09575b8b8ec89885c184fab4f2961fa4a
eleclerc85 <eleclerc@hotmail.ca>
2023-10-30 18:22:53 -0400

    PHP 8 bug mb_http_input

--- src/head.php
+++ src/head.php
@@ -3,8 +3,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 mb_internal_encoding('UTF-8');
-mb_http_output('UTF-8');
-mb_http_input('UTF-8');
+//mb_http_output('UTF-8');
+//mb_http_input('UTF-8'); // Bug in PHP
 mb_language('uni');
 mb_regex_encoding('UTF-8');
 
