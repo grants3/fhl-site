0ec561a34c3b04dc9a81bf2ad342d1861dd9d646
eleclerc85 <eleclerc@hotmail.ca>
2023-11-04 13:28:56 -0400

    Update PlayerVitalsHolder.php

--- src/classes/PlayerVitalsHolder.php
+++ src/classes/PlayerVitalsHolder.php
@@ -43,7 +43,7 @@ class PlayerVitalsHolder {
         //$i = 0;
         
         foreach ($contents as $cle => $val) {
-            $val = utf8_encode($val);
+            $val = encodeToUtf8($val);
             if(substr_count($val, '<P>(As of')){
                 $pos = strpos($val, ')');
                 $pos = $pos - 10;
