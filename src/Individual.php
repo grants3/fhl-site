6b0072f2f38b666dc357c9681461f4d1de833747
eleclerc85 <eleclerc@hotmail.ca>
2023-11-04 13:35:48 -0400

    Update Individual.php

--- src/Individual.php
+++ src/Individual.php
@@ -42,7 +42,7 @@ if (file_exists($Fnm)) {
 	$tableau = file($Fnm);
 
 	foreach ($tableau as $cle => $val) {
-		$val = utf8_encode($val);
+		$val = encodeToUtf8($val);
 		if(substr_count($val, '<P>(As of')){
 			$pos = strpos($val, ')');
 			$pos = $pos - 10;
