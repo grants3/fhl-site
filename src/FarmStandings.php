72424b5213f193b3f709fbf8aa02db4e6a52a981
eleclerc85 <eleclerc@hotmail.ca>
2023-11-04 13:34:28 -0400

    Update FarmStandings.php

--- src/FarmStandings.php
+++ src/FarmStandings.php
@@ -35,7 +35,7 @@ $lastUpdated='';
 if(file_exists($Fnm)) {
 	$tableau = file($Fnm);
 	while(list($cle,$val) = myEach($tableau)) {
-		$val = utf8_encode($val);
+		$val = encodeToUtf8($val);
 		if(substr_count($val, '<P>(As of')){
 			$pos = strpos($val, ')');
 			$pos = $pos - 10;
